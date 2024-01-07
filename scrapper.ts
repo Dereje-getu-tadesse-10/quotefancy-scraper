05fa6d3a9e66c3e1f59787a4555c37b069de0638
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-01-07 14:58:45 +0100

    feat: update getQuotes function for include quote image

--- scrapper.ts
+++ scrapper.ts
@@ -1,21 +1,28 @@
 import cheerio from "cheerio";
 
-type Quotes = {
+type Quote = {
   text: string;
   url: string;
+  image: string;
 };
 
-export const getQuotes = (html: string): { text: string; url: string }[] => {
+export const getQuotes = (html: string): Quote[] => {
   const $ = cheerio.load(html);
+  const quotes: Quote[] = [];
 
-  const quotes: Quotes[] = [];
+  $(".q-container").each((_, quoteContainer) => {
+    const text = $(quoteContainer).find("a.quote-a").text(); // quote text
+    const url = $(quoteContainer).find("a.quote-a").attr("href") ?? ""; // quote url
 
-  // select all <a>  with class "quote-a"
-  $("a.quote-a").each((i, elem) => {
-    const text = $(elem).text(); // get the text
-    const url = $(elem).attr("href") ?? ""; // get the url quote
-    quotes.push({ text, url }); // push in quotes array
+    // get image container
+    const wallpaper = $(quoteContainer).prev(".wallpaper");
+    const imageSrc = wallpaper.find("img").attr("src") ?? "";
+    const imageDataOriginal = wallpaper.find("img").attr("data-original") ?? "";
+    const image = imageSrc || imageDataOriginal; // use src or meta data-original
+
+    if (text && url && image) {
+      quotes.push({ text, url, image }); // push in quotes array
+    }
   });
-
   return quotes; // return quotes array
 };
