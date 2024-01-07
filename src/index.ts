e99edc27b2ab691ba198d5e3e5036eabfff25b00
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-01-07 19:35:32 +0100

    feat: create index route for quote

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,40 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
+
+import { fetchHtml } from "./fetch-html";
+import { getQuotes } from "./scrapper";
+import { writeQuote } from "./writeQuote";
+
 const app: Express = express();
+app.use(express.json());
+
 const port = 8080;
+
+app.post("/", (req: Request, res: Response) => {
+  const { path, file_name, file_type } = req.body;
+
+  if (!path || !file_name || !file_type) {
+    return res.status(400).json({
+      status: false,
+      message: "Missing required fields",
+    });
+  }
+
+  fetchHtml(path)
+    .then(getQuotes)
+    .then((quotes) => {
+      writeQuote(quotes, file_type, file_name);
+      res.json({
+        status: true,
+        message: "Quotes fetched and written successfully",
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        status: false,
+        message: "An error occurred",
+      });
+    });
+});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
