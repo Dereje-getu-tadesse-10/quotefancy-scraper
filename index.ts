b4c4cb081450d23da7061f0e3b0ca4fd512fe1a8
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-01-07 15:24:04 +0100

    feat: init the script

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello from index.ts");
+import { fetchHtml } from "./fetch-html";
+import { getQuotes } from "./scrapper";
+import { writeQuote } from "./writeQuote";
+
+const SCOPED_ARTIST = "/taylor-swift-quotes";
+const OUTPUT_FILE = "json";
+
+fetchHtml(SCOPED_ARTIST)
+  .then(getQuotes)
+  .then((quotes) => {
+    writeQuote(quotes, OUTPUT_FILE);
+  })
+  .catch((error) => console.log(error));
