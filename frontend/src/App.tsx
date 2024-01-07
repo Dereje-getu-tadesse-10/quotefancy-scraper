b17c962f44469033d572e22d3a021bbd744837c2
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-01-07 20:47:57 +0100

    style: hero section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import { Hero } from "./components/hero";
+
 function App() {
-  return <></>;
+  return (
+    <main className="max-w-[800px] mx-auto">
+      <Hero />
+    </main>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/hero.tsx
@@ -0,0 +1,10 @@
+export const Hero = () => (
+  <section className="my-20">
+    <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl">
+      Quotefancy Scrapper
+    </h1>
+    <p className="max-w-[600px] mx-auto mt-4 text-center text-lg text-muted-foreground sm:text-xl">
+      To get started you need the url of the quote.
+    </p>
+  </section>
+);
