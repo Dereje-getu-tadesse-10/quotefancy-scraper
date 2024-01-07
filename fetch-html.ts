d4a7a8a57600c6e37d608af4330e33ac541b2b3a
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-01-07 13:57:13 +0100

    update: create function fetchHtml

---
+++ fetch-html.ts
@@ -0,0 +1,26 @@
+import https from "node:https";
+
+export const fecthHtml = async (path: string): Promise<string> =>
+  new Promise((resolve, reject) => {
+    https
+      .get(
+        {
+          hostname: "quotefancy.com",
+          path, //  example of path: {{hostname}}/taylor-swift-quotes
+          method: "GET",
+        },
+        (res) => {
+          let html = ""; // store chunck data
+          res.on("data", function (chunk) {
+            html += chunk; // append the chunk
+          });
+          res.on("end", function () {
+            resolve(html); // resolve the promise with the response
+          });
+        }
+      )
+      .on("error", (error) => {
+        console.error(error);
+        reject(error);
+      });
+  });
