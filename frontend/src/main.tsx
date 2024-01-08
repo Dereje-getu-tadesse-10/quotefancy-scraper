53cff1617c98ac5247d6fc6b2e68b3eeba935560
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-01-08 20:07:10 +0100

    style: change the default theme by default

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <TooltipProvider>
           <ThemeSwitch />
           <App />
