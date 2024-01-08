20b373a1ab51fc23a39b08766de43873f3ef40ad
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-01-08 21:49:49 +0100

    feat: add textarea component for Multiple URL Scraping

--- frontend/src/components/form.tsx
+++ frontend/src/components/form.tsx
@@ -6,6 +6,8 @@ import { usePostScraperInfo } from "@/hooks/usePostScraperInfo";
 import { useDownloadStore } from "@/stores/useDownloadStore";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import { Textarea } from "@/components/ui/textarea";
 
 import {
   Form,
@@ -26,8 +28,10 @@ import {
 } from "@/components/ui/select";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useState } from "react";
 
 export const ScraperForm = () => {
+  const [showTextArea, setShowTextArea] = useState(false);
   const { addDownloadUrl } = useDownloadStore();
 
   const form = useForm<FormSchema>({
@@ -51,27 +55,68 @@ export const ScraperForm = () => {
       <CardContent>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField
-              control={form.control}
-              name="path"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Quotefancy url</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="https://quotefancy.com/taylor-swift-quotes, https://quotefancy.com/taylor-swift-quotes/page/2"
-                      {...field}
-                      type="url"
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    You can pass a list of url separed by <strong>comma</strong>
-                    .{" "}
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            {showTextArea ? (
+              <FormField
+                control={form.control}
+                name="path"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex justify-between items-center flex-wrap gap-4">
+                      Quotefancy url
+                      <div className="flex items-center gap-2">
+                        <p>Multiple URL Scraping</p>
+                        <Switch
+                          onClick={() => setShowTextArea(!showTextArea)}
+                        />
+                      </div>
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder={`https://quotefancy.com/taylor-swift-quotes,
+https://quotefancy.com/taylor-swift-quotes/page/2,
+https://quotefancy.com/taylor-swift-quotes/page/3
+`}
+                        {...field}
+                        className="resize-none"
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      You can pass a list of url separed by{" "}
+                      <strong>comma</strong>.{" "}
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ) : (
+              <FormField
+                control={form.control}
+                name="path"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex justify-between items-center flex-wrap gap-4">
+                      Quotefancy url
+                      <div className="flex items-center gap-2">
+                        <p>Multiple URL Scraping</p>
+                        <Switch
+                          onClick={() => setShowTextArea(!showTextArea)}
+                        />
+                      </div>
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="https://quotefancy.com/taylor-swift-quotes"
+                        {...field}
+                        type="url"
+                      />
+                    </FormControl>
+                    <FormDescription>quotefancy url </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+
             <FormField
               control={form.control}
               name="file_name"
